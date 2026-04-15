97aca371b4f0f7e44375839956b85dd24597121a
themdshakibul <mdshakibul88240@gmail.com>
2026-04-15 20:56:50 +0600

    heder page total friend length show redmi md create

--- src/Components/HomePage/TotalDetails.jsx
+++ src/Components/HomePage/TotalDetails.jsx
@@ -1,22 +1,29 @@
+import { getFriends } from "@/lib/friends";
+
 const TotalDetails = async () => {
+  const friends = await getFriends();
+
+  const friendOntrack = friends.filter((friend) => friend.status == "on_track");
+  const friendAlon = friends.filter((friend) => friend.status == "overdue");
+
   return (
     <section>
       <div className="container mx-auto px-2 grid md:grid-cols-2 lg:grid-cols-4 gap-10 mb-10">
         <div className="flex flex-col space-y-3 items-center justify-center bg-base-300 border border-gray-300 p-5 py-10 rounded-md">
           <h2 className=" text-4xl md:text-5xl font-black text-[#244d3f]">
-            {9}
+            {friends.length}
           </h2>
           <p className="text-2xl font-semibold opacity-90">Total Friends</p>
         </div>
         <div className="flex flex-col space-y-3 items-center justify-center bg-base-300 border border-gray-300 p-5  py-10 rounded-md">
           <h2 className=" text-4xl md:text-5xl font-black text-[#244d3f]">
-            {3}
+            {friendOntrack.length}
           </h2>
           <p className="text-2xl font-semibold opacity-90">On Track</p>
         </div>
         <div className="flex flex-col space-y-3 items-center justify-center bg-base-300 border border-gray-300 p-5 py-10 rounded-md">
           <h2 className=" text-4xl md:text-5xl font-black text-[#244d3f]">
-            {6}
+            {friendAlon.length}
           </h2>
           <p className="text-2xl font-semibold opacity-90">Need Attention</p>
         </div>
